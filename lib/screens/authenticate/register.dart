27e0b78718ca440253ce30840c8afad4f4e16ed6
Priyanshu <rpriyanshu9@gmail.com>
2020-08-28 11:02:02 +0530

    Fixed TextFormField hidden by keyboard

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -46,64 +46,82 @@ class _RegisterState extends State<Register> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                  //here string is the helper texxt and null says that it is valid
-                  validator: (val)=> val.isEmpty ? 'Enter an email': null,
-                  // this val represents whatever is inside the form field at that time
-                  // and this onChanged will trigger when ever this val changes
-                  onChanged: (val) {
-                    setState(() {
-                      email = val;
-                    });
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                  //here string is the helper texxt and null says that it is valid
-                validator: (val) => val.length<6 ? 'Password must be atleat 6 chars long': null,
-                  //obscures means to hide the password as you type
-                  obscureText: true,
-                  onChanged: (val) {
-                    password = val;
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                RaisedButton(
-                  color: Colors.black26,
-                  child: Text(
-                    "Register",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  // this is async as sign in results in using firebase utilities and that might take sometime
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()){
-                      setState(()=>loading=true);
-                      // for this to work we need individual validators on the form fields
-                      //print("$email and $password");
-                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                      // null if error, user if success
-                      if (result==null){
-                        setState((){loading=false;error='please supply a valid email';});
-                      }
-                    }
-                  },
-                )
-              ],
-            )),
-      ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration:
+                                textInputDecoration.copyWith(hintText: 'Email'),
+                            //here string is the helper texxt and null says that it is valid
+                            validator: (val) =>
+                                val.isEmpty ? 'Enter an email' : null,
+                            // this val represents whatever is inside the form field at that time
+                            // and this onChanged will trigger when ever this val changes
+                            onChanged: (val) {
+                              setState(() {
+                                email = val;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Password'),
+                            //here string is the helper texxt and null says that it is valid
+                            validator: (val) => val.length < 6
+                                ? 'Password must be atleat 6 chars long'
+                                : null,
+                            //obscures means to hide the password as you type
+                            obscureText: true,
+                            onChanged: (val) {
+                              password = val;
+                            },
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          RaisedButton(
+                            color: Colors.black26,
+                            child: Text(
+                              "Register",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            // this is async as sign in results in using firebase utilities and that might take sometime
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                setState(() => loading = true);
+                                // for this to work we need individual validators on the form fields
+                                //print("$email and $password");
+                                dynamic result =
+                                    await _auth.registerWithEmailAndPassword(
+                                        email, password);
+                                // null if error, user if success
+                                if (result == null) {
+                                  setState(() {
+                                    loading = false;
+                                    error = 'please supply a valid email';
+                                  });
+                                }
+                              }
+                            },
+                          )
+                        ],
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom))
+                ],
+              ),
+            ),
     );
   }
 }
